1cf9c00d832017661fd160385b360a1344ca637c
Lucas Anacleto <lucasanacleto04@gmail.com>
2025-10-17 15:10:02 -0300

    Integrando React Hook Form e Zod com o formulário de SignUp

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,15 +8,55 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field";
+import { Field, FieldDescription, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+
+const schema = z
+  .object({
+    fullname: z.string().min(1, "Informe o seu nome completo"),
+    email: z.string().email("Informe um e-mail válido"),
+    password: z.string().min(8, "A senha deve ter pelo menos 8 caracteres"),
+    confirm_password: z.string().min(8, "Confirme a senha"),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password !== data.confirm_password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "As senhas não coincidem",
+        path: ["confirm_password"],
+      });
+    }
+  });
+
+type FormData = z.infer<typeof schema>;
 
 export function SignUpForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const form = useForm<FormData>({
+    defaultValues: {
+      fullname: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = form.handleSubmit((formData) => {
+    console.log(formData);
+  });
+
   return (
     <Card {...props}>
       <CardHeader>
@@ -24,51 +66,90 @@ export function SignUpForm({ ...props }: React.ComponentProps<typeof Card>) {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="name">Nome completo</FieldLabel>
-              <Input id="name" type="text" placeholder="John Doe" required />
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="email">E-mail</FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-              <FieldDescription>
-                Nós usaremos para nos comunicar com você. Não compartilhamos seu
-                e-mail com ninguem.
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Senha</FieldLabel>
-              <Input id="password" type="password" required />
-              <FieldDescription>
-                Deve possuir mais de 8 caractéres
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="confirm-password">
-                Confirmar senha
-              </FieldLabel>
-              <Input id="confirm-password" type="password" required />
-              <FieldDescription>Você deve confirmar sua senha</FieldDescription>
-            </Field>
+        <form onSubmit={handleSubmit}>
+          <Form {...form}>
             <FieldGroup>
-              <Field>
-                <Button type="submit">Criar conta</Button>
-                <Button variant="outline" type="button">
-                  Criar conta com o Google
-                </Button>
-                <FieldDescription className="px-6 text-center">
-                  Já tem uma conta? <a href="/signin">Acesse agora!</a>
-                </FieldDescription>
-              </Field>
+              <FormField
+                control={form.control}
+                name="fullname"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome completo</FormLabel>
+                    <FormControl>
+                      <Input placeholder="John Doe" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome completo</FormLabel>
+                    <FormControl>
+                      <Input placeholder="m@example.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription>
+                      Será usado para nos comunicarmos com você. Não
+                      compartilhamos seu e-mail com ninguem.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="********"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="confirm_password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmar senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="********"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FieldGroup>
+                <Field>
+                  <Button type="submit">Criar conta</Button>
+                  <Button variant="outline" type="button">
+                    Criar conta com o Google
+                  </Button>
+                  <FieldDescription className="px-6 text-center">
+                    Já tem uma conta? <a href="/signin">Acesse agora!</a>
+                  </FieldDescription>
+                </Field>
+              </FieldGroup>
             </FieldGroup>
-          </FieldGroup>
+          </Form>
         </form>
       </CardContent>
     </Card>
