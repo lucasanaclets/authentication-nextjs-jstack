3a17b9b0ea195c04f71710acd39e7342c9ff74fd
Lucas Anacleto <lucasanacleto04@gmail.com>
2025-10-20 19:39:27 -0300

    Consultando no banco de dados as informações do usuário autenticado

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { env } from "@/config/env";
 import { JwtPayload, verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
+import { prismaClient } from "./prismaClient";
+import { User } from "@/entities/User";
 
 async function getAccessToken() {
   return (await cookies()).get("accessToken")?.value;
@@ -31,6 +33,21 @@ export async function isAuthenticated() {
   return !!verify;
 }
 
-export function auth(): Promise<null | string> {
-  return verifyJwt();
+export async function auth(): Promise<User | null> {
+  const userId = await verifyJwt();
+
+  if (!userId) {
+    return null;
+  }
+
+  try {
+    const user = await prismaClient.user.findUnique({
+      where: { id: userId },
+      select: { id: true, fullname: true, email: true },
+    });
+
+    return user;
+  } catch {
+    return null;
+  }
 }
